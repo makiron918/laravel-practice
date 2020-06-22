b70d4e17caa670b0169ae73a8c8cbdbd6df05d3a
makiron918 <gonzaress918@gmail.com>
2020-06-22 18:41:10 +0900

    Update category.blade.php

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,9 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>newsの一覧</title>
+  <title>{{ $category }}の一覧</title>
 </head>
 <body>
-  <p>newsの一覧</p>
+  <p>{{ $category }}の一覧</p>
 </body>
 </html>
