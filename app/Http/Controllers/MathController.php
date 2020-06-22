6536c86b62e1441ceddb09b1ea4fc7ff6e665f78
makiron918 <gonzaress918@gmail.com>
2020-06-22 22:51:35 +0900

    Update MathController.php

--- app/Http/Controllers/MathController.php
+++ app/Http/Controllers/MathController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class MathController extends Controller
 {
@@ -10,6 +11,6 @@ class MathController extends Controller
     public function sum($x, $y) {
         $sum = $x + $y;
 
-        return View::make('sum', ['x'=>$x, 'y'=>$y, 'sum'=>$sum])
+        return View::make('sum', ['x'=>$x, 'y'=>$y, 'sum'=>$sum]);
     }
 }
